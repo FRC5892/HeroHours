74ab89c823b367a4a89e8ef5810c4cab05f0415d
Michael Statt <117385299+MichaelStatt-S@users.noreply.github.com>
2024-10-29 18:53:50 -0500

    it works now

--- HeroHours_api/views.py
+++ HeroHours_api/views.py
@@ -7,13 +7,11 @@ from rest_framework.settings import api_settings
 from rest_framework_csv import renderers as csv_renderers
 
 from HeroHours.models import Users
-from HeroHours_api.serializers import TodoSerializer
-
 
 # Create your views here.
 class TodoListApiView(APIView):
-    renderer_classes = (csv_renderers.CSVRenderer,) + tuple(api_settings.DEFAULT_RENDERER_CLASSES)
-    @renderer_classes((TodoSerializer))
+    renderer_classes = [csv_renderers.CSVRenderer]
+
     def get(self, request, *args, **kwargs):
-        members = Users.objects.all()
-        return Response(serializer.render(data=members), status=status.HTTP_200_OK)
+        members = list(Users.objects.all().values())
+        return Response(members, status=status.HTTP_200_OK)
